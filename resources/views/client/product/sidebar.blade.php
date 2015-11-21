3502b8ed5a8db153afe8d3856c2a21a9fda5e004
chile <lechiit@gmail.com>
2015-11-21 10:59:21 +0700

    KCN-39 #comment + remove option filter when click twice.

--- resources/views/client/product/sidebar.blade.php
+++ resources/views/client/product/sidebar.blade.php
@@ -12,24 +12,34 @@
                     </div>
                     <materializer data-show-height="computed" data-hide-height="0" id="as-accordion-id0" class="as-search-facet-materializer ase-materializer ase-materializer-show" data-shown-init="true">
                         <div class="as-accordion-box-animation">
-                            <ul class="as-search-filter-items as-filter-text-type">
+                            <ul class="as-search-filter-items as-filter-text-type tag">
                                 @forelse($links as $link)
                                 <li class="as-filter-item">
-                                    <a href="{{$link['tag_url']}}?page=1&tag={{$link['id']}}" class="as-filter-option" aria-disabled="false" tabindex="0" data-tracking="accType=controller, " role="checkbox" aria-checked="false">
+                                    @if(sizeof($link['childs'])>0)
+                                        <a href="javascript:void(0)">
+                                            <span class="as-filter-name">
+                                                <span class="as-search-filter-content">
+                                                    <span class="as-search-filter-text">{{$link['name']}}
+                                                        @if(sizeof($link['childs'])>0)
+                                                            <i class="as-search-filter-child{{$link['id']}} fa fa-angle-down"></i>
+                                                        @endif
+                                                    </span>
+                                               </span>
+                                           </span>
+                                        </a>
+                                    @else
+                                        <a href="{{$link['tag_url']}}?page=1&tag={{$link['id']}}" class="as-filter-option" aria-disabled="false" tabindex="0" data-type="tag" role="checkbox" aria-checked="false">
                                         <span class="as-filter-name">
                                             <span class="as-search-filter-content">
-                                                <span class="as-search-filter-text">{{$link['name']}}
-                                                    @if(sizeof($link['childs'])>0)
-                                                        <i class="as-search-filter-child{{$link['id']}} fa fa-angle-down"></i>
-                                                    @endif
-                                                </span>
+                                                <span class="as-search-filter-text">{{$link['name']}}</span>
                                            </span>
                                        </span>
-                                    </a>
+                                        </a>
+                                    @endif
                                     @if(isset($link['childs']) && sizeof($link['childs'])>0)
-                                        <ul id="as-seach-filter-childs{{$link['id']}}" class="as-search-filter-childs ase-materializer-gone">
-                                            <li class="as-filter-child-item">
-                                                <a href="{{$link['tag_url']}}?page=1&tag={{$link['id']}}" data-type="tag={{$link['id']}}" class="as-filter-option" aria-disabled="false" tabindex="0" role="checkbox" aria-checked="false">
+                                        <ul id="as-seach-filter-childs{{$link['id']}}" class="as-search-filter-childs ase-materializer-gone tag">
+                                            <li class="as-filter-child-item as-filter-item">
+                                                <a href="{{$link['tag_url']}}?page=1&tag={{$link['id']}}" data-type="tag" class="as-filter-option" aria-disabled="false" tabindex="0" role="checkbox" aria-checked="false">
                                                     <span class="as-filter-name">
                                                         <span class="as-search-filter-content">
                                                             <span class="as-search-filter-text">Tất cả</span>
@@ -38,8 +48,8 @@
                                                 </a>
                                             </li>
                                         @foreach($link['childs'] as $child)
-                                            <li class="as-filter-child-item">
-                                                <a href="{{urlCategory($child)}}?page=1&tag={{$child->id}}" class="as-filter-option">
+                                            <li class="as-filter-child-item as-filter-item">
+                                                <a href="{{urlCategory($child)}}?page=1&tag={{$child->id}}" class="as-filter-option" data-type="tag" role="checkbox" aria-checked="false">
                                                     <span class="as-filter-name">
                                                         <span class="as-search-filter-content">
                                                             <span class="as-search-filter-text">{{$child->name}}</span>
@@ -74,7 +84,7 @@
                             <ul class="as-search-filter-items as-filter-text-type brand">
                                 @forelse($brands as $brand)
                                     <li class="as-filter-item">
-                                        <a href="{{Request::url()}}?page=1&brand={{$brand->id}}" data-type='brand={{$brand->id}}' class="as-filter-option" aria-disabled="false" tabindex="0" data-tracking="accType=controller, " role="checkbox" aria-checked="false">
+                                        <a href="{{Request::url()}}?page=1&brand={{$brand->id}}" data-type='brand' class="as-filter-option" aria-disabled="false" tabindex="0" data-tracking="accType=controller, " role="checkbox" aria-checked="false">
                                             <span class="as-filter-name">
                                                 <span class="as-search-filter-content">
                                                     <span class="as-search-filter-text">{{$brand->name}}</span>
@@ -102,10 +112,10 @@
                     </div>
                     <materializer data-show-height="computed" data-hide-height="0" id="as-accordion-id2" class="as-search-facet-materializer ase-materializer ase-materializer-gone ase-materializer-hide" data-shown-init="true">
                         <div class="as-accordion-box-animation">
-                            <ul class="as-search-filter-items as-filter-image-type as-search-color-links">
+                            <ul class="as-search-filter-items as-filter-image-type as-search-color-links color">
                                 @forelse($colors as $color)
                                     <li class="as-filter-item as-search-image-item colorselector-link">
-                                        <a href="{{Request::url()}}?page=1&color={{$color->id}}" class="as-filter-option" aria-disabled="false" tabindex="0" data-tracking="accType=controller, " role="checkbox" aria-checked="false">
+                                        <a href="{{Request::url()}}?page=1&color={{$color->id}}" class="as-filter-option" aria-disabled="false" tabindex="0" data-type="color" role="checkbox" aria-checked="false">
                                             <figure class="colorselector-swatch">
                                                 <span class="colorselector-circle" style="background-color:{{$color->code}}; border-radius: 50%; padding:4px 12px"></span>
                                                 <figcaption class="colorselector-caption">{{$color->name}}</figcaption>
