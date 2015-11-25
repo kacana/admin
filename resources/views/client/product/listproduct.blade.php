46553d50fa41c6205ff24cc87acd42bc138f75fa
chile <lechiit@gmail.com>
2015-11-26 00:15:06 +0700

    KCN-58#commit, fixed

--- resources/views/client/product/listproduct.blade.php
+++ resources/views/client/product/listproduct.blade.php
@@ -17,7 +17,7 @@
             </div>
             <div class="block-tag-body as-accessories-results">
                 @include('client.product.sidebar')
-                <div class="container column taglist" id="content">
+                <div class="container taglist" id="content">
                     @forelse($items as $item)
                         <div class="col-md-4 product-item" >
                             <div class="product-image" >
